c38ee56d8cb045a1f2fdeecf754c8a9807c49e09
Claire Southwell <47905732+ClaireSouthwell@users.noreply.github.com>
2019-11-24 20:47:41 -0500

    Added input validation
    Used try/except to validate that user input string can be parsed to an integer. Also rewrote main function to use recursion instead of previous workaround.

--- collatz.py
+++ collatz.py
@@ -1,17 +1,21 @@
 # Given a number, the collatz function will either divide it by 2 (if even) or multiply by 3 and add 1 (if odd) until 1 is reached.
 
 def collatz(number):
-    if number % 2 == 0:
-        number = number // 2
-        print(number)
-        return number
-    else:
-        number = 3 * number + 1
-        print(number)
-        return number
-    
+    try:
+        if int(number) % 2 == 0:
+            number = int(number) // 2
+            print(number)
+            if number > 1:
+                collatz(number)
+            return number
+        else:
+            number = 3 * int(number) + 1
+            print(number)
+            collatz(number)
+            return number
+    except ValueError:
+        print('Please enter an integer.')
+        
 print('Enter a number:')        
-userInput = int(input())
-newNumber = collatz(userInput)
-while newNumber > 1:
-    newNumber = collatz(newNumber)
+userInput = input()
+collatz(userInput)
